e91ca6a219a4092c5f437bd5c3f538e1e4f4a277
Michelle Wang <oc3anazns3a@gmail.com>
2020-12-22 01:52:50 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -4,3 +4,8 @@ function addElementToBeginningOfArray (arr, element) {
   var temp = [... arr];
   return temp.unshift(element);
 }
+
+function addElementToEndOfArray (arr, element) {
+  var temp = [... arr];
+  return temp.push(element);
+}
