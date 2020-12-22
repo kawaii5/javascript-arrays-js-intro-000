41d9f7b29494e80ff4d3ad69fc27d8c12429d225
Michelle Wang <oc3anazns3a@gmail.com>
2020-12-22 01:54:44 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -7,3 +7,11 @@ function addElementToBeginningOfArray (arr, element) {
 function addElementToEndOfArray (arr, element) {
   return [... arr, element];
 }
+
+function destructivelyAddElementToBeginningOfArray (arr, element) {
+  return arr.unshift(element);
+}
+
+function destructivelyAddElementToEndOfArray (arr, element) {
+  return arr.push(element);
+}
