3f9491d987b8f953c2ecf6fd3323fceb8945c22e
Michelle Wang <oc3anazns3a@gmail.com>
2020-12-22 01:51:29 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -2,5 +2,5 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray (arr, element) {
   var temp = [... arr];
-  return arr.shift(temp);
+  return temp.shift(element);
 }
